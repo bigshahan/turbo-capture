90da2606f1a5108bac84f1a5a15366cb776b056f
Shahan Khan <contact@shahan.me>
2014-09-04 23:05:19 -0700

    Initial Commit

---
+++ Video CaptureTests/Video_CaptureTests.swift
@@ -0,0 +1,36 @@
+//
+//  Video_CaptureTests.swift
+//  Video CaptureTests
+//
+//  Created by Shahan Khan on 9/4/14.
+//  Copyright (c) 2014 Shahan Khan. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Video_CaptureTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
